2c68a1090b9ee481193c94b0c23c6e27280775c9
SimonBjorkberg <S.bjorkberg@gmail.com>
2024-08-03 15:47:47 +0200

    fuk u css

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>CocoCaca</p>
+    <div className='h-dvh w-dvw bg-[#282c34] flex justify-center items-center'>
+      <header>
+        <p className='text-white text-2xl'>Allegory of the Rave</p>
       </header>
     </div>
   );
